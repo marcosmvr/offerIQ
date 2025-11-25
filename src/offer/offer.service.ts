155cab4e888413f113749c7e5c1c98801451c667
Marcos <marcosviniciusreis260@gmail.com>
2025-11-25 20:45:06 -0300

    feat(offer): creates the main offer management service

---
+++ src/offer/offer.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Injectable()
+export class OfferService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data) {
+    return await this.prisma.offer.create({
+      data,
+      include: { metrics: true },
+    })
+  }
+
+  async findByUserId(userId: string) {
+    return await this.prisma.offer.findMany({
+      where: { userId },
+      orderBy: { createdAt: 'desc' },
+    })
+  }
+
+  async findById(id: string, userId: string) {
+    const offer = await this.prisma.offer.findFirst({
+      where: { id, userId },
+      include: { metrics: true, reports: true },
+    })
+
+    if (!offer) {
+      throw new NotFoundException('Oferta não encontrada ou acesso negado.')
+    }
+
+    return offer
+  }
+
+  async updateById(id: string, userId: string, data) {
+    const offerCheck = await this.prisma.offer.findFirst({
+      where: { id, userId },
+    })
+
+    if (!offerCheck) {
+      throw new NotFoundException('Oferta não encontrada ou acesso negado.')
+    }
+
+    return await this.prisma.offer.update({
+      where: { id },
+      data,
+    })
+  }
+
+  async deleteById(id: string, userId: string) {
+    const offerCheck = await this.prisma.offer.findFirst({
+      where: { id, userId },
+    })
+
+    if (!offerCheck) {
+      throw new NotFoundException('Oferta não encontrada ou acesso negado.')
+    }
+
+    return this.prisma.offer.delete({
+      where: { id },
+    })
+  }
+}
