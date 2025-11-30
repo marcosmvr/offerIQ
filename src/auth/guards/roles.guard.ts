eadd1362c045759b6e36c96492d5e1f778ef33ec
Marcos <marcosviniciusreis260@gmail.com>
2025-11-29 22:04:15 -0300

    feat(roles): create roles guards and update path guards

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,40 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<string[]>(
+      'roles',
+      context.getHandler(),
+    )
+
+    if (!requiredRoles) {
+      return true
+    }
+
+    const request = context.switchToHttp().getRequest()
+    const user = request.user
+
+    if (!user) {
+      throw new ForbiddenException('Usuário não autenticado.')
+    }
+
+    const hasRole = requiredRoles.some(role => user.role === role)
+
+    if (!hasRole) {
+      throw new ForbiddenException(
+        'Você não tem permissão para executar esta ação.',
+      )
+    }
+
+    return true
+  }
+}
