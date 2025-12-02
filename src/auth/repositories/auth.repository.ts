cd566a4edc57499bdf54b866f2bdfd72e3ceabdf
Marcos <marcosviniciusreis260@gmail.com>
2025-12-02 20:23:43 -0300

    feat(auth): implement AuthRepository with Prisma

---
+++ src/auth/repositories/auth.repository.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../../prisma/prisma.service'
+import { Prisma, User } from '@prisma/client'
+import { IAuthRepository } from './auth.repository.interface'
+
+@Injectable()
+export class AuthRepository implements IAuthRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { email },
+    })
+  }
+
+  async findById(userId: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { id: userId },
+    })
+  }
+
+  async emailExists(email: string): Promise<boolean> {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+      select: { id: true },
+    })
+    return !!user
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    })
+  }
+}
