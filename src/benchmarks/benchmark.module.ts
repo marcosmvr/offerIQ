83330b636486f5b1dbc4ee7b82d22f41656bd575
Marcos <marcosviniciusreis260@gmail.com>
2025-12-02 20:58:07 -0300

    chore(benchmark): wire repository dependecy injection

--- src/benchmarks/benchmark.module.ts
+++ src/benchmarks/benchmark.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common'
 import { BenchmarksController } from './benchmark.controller'
 import { BenchmarkService } from './benchmark.service'
 import { PrismaModule } from '../prisma/prisma.module'
+import { BENCHMARK_REPOSITORY } from './repositories/benchmark.repository.interface'
+import { BenchmarkRepository } from './repositories/benchmark.repository'
 
 @Module({
   imports: [PrismaModule],
   controllers: [BenchmarksController],
-  providers: [BenchmarkService],
+  providers: [
+    BenchmarkService,
+    {
+      provide: BENCHMARK_REPOSITORY,
+      useClass: BenchmarkRepository,
+    },
+  ],
   exports: [BenchmarkService],
 })
 export class BenchmarksModule {}
