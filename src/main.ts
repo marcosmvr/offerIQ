8ff98a30943b1394ccc0e82a99f733578f8930ed
Marcos <marcosviniciusreis260@gmail.com>
2026-02-09 20:50:47 -0300

    chore: add port 0.0.0.0 to render

--- src/main.ts
+++ src/main.ts
@@ -61,7 +61,7 @@ async function bootstrap() {
   })
 
   const port = process.env.PORT || 3000
-  await app.listen(port)
+  await app.listen(port, '0.0.0.0')
 
   console.log(`🚀 Aivo API running on http://localhost:${port}`)
   console.log(`📚 Swagger docs: http://localhost:${port}/docs`)
